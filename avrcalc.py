cb83b51bfdac3ec0d803363ecccc6551f6815ae5
oivalian <109859213+oivalian@users.noreply.github.com>
2024-10-15 13:14:22 +1100

    Update avrcalc.py

--- avrcalc.py
+++ avrcalc.py
@@ -4,4 +4,4 @@ days = int(input('Days to calculate > '))
 [minutes.append(int(input('Enter daily minute value > '))) for _ in range(days)]
 mean = sum(minutes) / len(minutes)
 
-print(f"Your average screentime is {mean} minutes")
+print(f"Your average screentime is {mean:.0f} minutes")
