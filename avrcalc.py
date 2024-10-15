08b8dac3b8627b2cc1f89e59ad4de10cabda35e4
oivalian <109859213+oivalian@users.noreply.github.com>
2024-10-15 13:03:30 +1100

    Simplified code

--- avrcalc.py
+++ avrcalc.py
@@ -1,11 +1,7 @@
-import numpy as np
+minutes = []
+days = int(input('Days to calculate > '))
 
-arr = []
-app_name = input('What app are you using > ')
-array_size = int(input('Days to calculate > '))
+[minutes.append(int(input('Enter daily minute value > '))) for _ in range(days)]
+mean = sum(minutes) / len(minutes)
 
-for value in range(array_size):
-    arr.append(input('Enter daily minute value > '))
-
-arr = np.array(arr).astype(int)
-print(f'Your average screentime for {app_name} is {np.mean(arr)} minutes')
+print(f"Your average screentime is {mean} minutes")
